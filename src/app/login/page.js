a705ec18060a73bc83d09a69cd78ec8632c40245
rockomatthews <rob@robmatthe.ws>
2024-10-10 12:00:02 -0600

    Got google auth to work and create a user

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,24 +1,59 @@
-// src/app/login/page.js
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Typography, TextField, IconButton } from '@mui/material';
 import { Google as GoogleIcon, Facebook as FacebookIcon, Apple as AppleIcon } from '@mui/icons-material';
 import SportsMotorsportsIcon from '@mui/icons-material/SportsMotorsports';
 import { useRouter } from 'next/navigation';
 import loginBackground from '../../public/loginBackground.png';
+import { auth } from '../../config/firebase';
+
+import { 
+  signInWithPopup, 
+  GoogleAuthProvider, 
+  FacebookAuthProvider,
+  signInWithEmailAndPassword
+} from "firebase/auth";
 
 const LoginPage = () => {
   const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  // Placeholder for actual authentication logic later
-  const handleSocialLogin = (provider) => {
-    console.log(`Logging in with ${provider}`);
-    // Placeholder logic for social login
+  const handleSocialLogin = async (provider) => {
+    try {
+      let authProvider;
+      switch(provider) {
+        case 'Google':
+          authProvider = new GoogleAuthProvider();
+          break;
+        case 'Facebook':
+          authProvider = new FacebookAuthProvider();
+          break;
+        default:
+          console.error('Unsupported provider');
+          return;
+      }
+      
+      const result = await signInWithPopup(auth, authProvider);
+      console.log(`Logged in with ${provider}`, result.user);
+      // Redirect or update UI after successful login
+      router.push('/dashboard');
+    } catch (error) {
+      console.error(`${provider} login failed`, error);
+      // Handle errors here, such as displaying error messages
+    }
   };
 
-  const handleEmailLogin = () => {
-    console.log('Logging in with Email');
-    // Placeholder logic for email login
+  const handleEmailLogin = async () => {
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      console.log('Logged in with email', result.user);
+      // Redirect or update UI after successful login
+      router.push('/dashboard');
+    } catch (error) {
+      console.error('Email login failed', error);
+      // Handle errors here, such as displaying error messages
+    }
   };
 
   return (
@@ -37,7 +72,7 @@ const LoginPage = () => {
     >
       <Box
         sx={{
-          backgroundColor: 'rgba(0, 0, 0, 0.8)', // Dark overlay for better text contrast
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
           padding: '30px',
           borderRadius: '10px',
           width: '100%',
@@ -45,13 +80,11 @@ const LoginPage = () => {
           textAlign: 'center',
         }}
       >
-        {/* Sign Up Header */}
         <Typography variant="h4" gutterBottom sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: 5 }}>
           <SportsMotorsportsIcon alt="Helmet Icon" style={{ width: 50, height: 50, marginBottom: 30 }} />
           Sign In
         </Typography>
 
-        {/* Social Login Options */}
         <Box sx={{ display: 'flex', justifyContent: 'center', gap: '10px', marginBottom: '20px' }}>
           <IconButton onClick={() => handleSocialLogin('Google')} sx={{ backgroundColor: '#4285F4', color: '#fff' }}>
             <GoogleIcon />
@@ -59,7 +92,7 @@ const LoginPage = () => {
           <IconButton onClick={() => handleSocialLogin('Facebook')} sx={{ backgroundColor: '#3b5998', color: '#fff' }}>
             <FacebookIcon />
           </IconButton>
-          <IconButton onClick={() => handleSocialLogin('Apple')} sx={{ backgroundColor: '#000', color: '#fff' }}>
+          <IconButton sx={{ backgroundColor: '#000', color: '#fff' }}>
             <AppleIcon />
           </IconButton>
         </Box>
@@ -68,22 +101,66 @@ const LoginPage = () => {
           or
         </Typography>
 
-        {/* Email and Password Fields */}
         <TextField
           label="Email"
           variant="outlined"
           fullWidth
-          sx={{ marginBottom: '15px', backgroundColor: '#fff', borderRadius: '5px' }}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          sx={{
+            marginBottom: '15px',
+            backgroundColor: '#fff',
+            borderRadius: '5px',
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'rgba(0, 0, 0, 0.23)',
+              },
+              '&:hover fieldset': {
+                borderColor: 'rgba(0, 0, 0, 0.5)',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: '#000',
+              },
+            },
+            '& .MuiInputBase-input': {
+              color: '#000',
+            },
+            '& .MuiInputLabel-root': {
+              color: 'rgba(0, 0, 0, 0.6)',
+            },
+          }}
         />
         <TextField
           label="Password"
           type="password"
           variant="outlined"
           fullWidth
-          sx={{ marginBottom: '15px', backgroundColor: '#fff', borderRadius: '5px' }}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          sx={{
+            marginBottom: '15px',
+            backgroundColor: '#fff',
+            borderRadius: '5px',
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'rgba(0, 0, 0, 0.23)',
+              },
+              '&:hover fieldset': {
+                borderColor: 'rgba(0, 0, 0, 0.5)',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: '#000',
+              },
+            },
+            '& .MuiInputBase-input': {
+              color: '#000',
+            },
+            '& .MuiInputLabel-root': {
+              color: 'rgba(0, 0, 0, 0.6)',
+            },
+          }}
         />
 
-        {/* Login Button */}
         <Button
           variant="contained"
           color="primary"
@@ -91,7 +168,7 @@ const LoginPage = () => {
           sx={{ marginTop: '15px', backgroundColor: '#FFC107', color: '#000' }}
           onClick={handleEmailLogin}
         >
-          Next
+          Sign In
         </Button>
       </Box>
     </Box>
