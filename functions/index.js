b4283bca7a01de97887cf288ed599253020f31fb
rockomatthews <rob@robmatthe.ws>
2024-11-15 15:52:41 -0700

    try 4 with clean package.json

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,16 @@
-const { onRequest } = require('firebase-functions/v2/https');
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 
 admin.initializeApp();
-const app = express();
 
+const app = express();
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
+app.get('/test', (req, res) => {
   res.json({ message: 'Hello from Firebase!' });
 });
 
-// Export the 2nd gen function
-exports.api = onRequest({
-  region: 'us-central1',
-  memory: '256MiB',
-  minInstances: 0,
-  maxInstances: 10,
-}, app);
-
-// If you have other functions, make sure they're all 2nd gen
-exports.hello = onRequest({
-  region: 'us-central1',
-  memory: '256MiB',
-}, (req, res) => {
-  res.send('Hello from Firebase v2!');
-});
+exports.api = functions.https.onRequest(app);
