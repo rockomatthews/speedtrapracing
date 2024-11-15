074e1bf9331529c06f076e5a400d2047e190f1e8
rockomatthews <rob@robmatthe.ws>
2024-11-15 13:50:34 -0700

    add fixes 3 for deploy

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
-const functions = require('firebase-functions');
+const { onRequest } = require('firebase-functions/v2/https');
 const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
 
 admin.initializeApp();
+const app = express();
 
-exports.hello = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello from Firebase!' });
+});
+
+// Export the 2nd gen function
+exports.api = onRequest({
+  region: 'us-central1',
+  memory: '256MiB',
+  minInstances: 0,
+  maxInstances: 10,
+}, app);
+
+// If you have other functions, make sure they're all 2nd gen
+exports.hello = onRequest({
+  region: 'us-central1',
+  memory: '256MiB',
+}, (req, res) => {
+  res.send('Hello from Firebase v2!');
 });
